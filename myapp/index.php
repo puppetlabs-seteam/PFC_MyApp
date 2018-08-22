8dedc194d380f272627c8ed0085b23bb67884f00
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-08-22 11:24:23 +0200

    Update index.php

--- myapp/index.php
+++ myapp/index.php
@@ -152,7 +152,7 @@ if (isset($_SESSION['username']))
 	// This section is shown when user is login
 	echo "<table width=100% border=0>";
 	echo "<tr>";
-		echo "<td><H1>Welcome to the Pipelines for Applications Sample App!</H1></td>";
+		echo "<td><H1>Welcome DXC to the Pipelines for Applications Sample App!</H1></td>";
 		echo "<td align='right'>";
 			echo "$username<br>";
 			echo "<a href='index.php?logout=yes'>Logout</a>";
